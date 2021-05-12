1922b74d5771615f8c0613006dbce386779b4b67
Haoming <nh.haoming@gmail.com>
2021-05-12 09:26:28 +0900

    メニューの表示を変更

--- Source Code/CustomOptions.cs
+++ Source Code/CustomOptions.cs
@@ -593,10 +593,10 @@ namespace TheOtherRoles {
                 gap = 5;
                 index = hudString.TakeWhile(c => (gap -= (c == '\n' ? 1 : 0)) > 0).Count();
                 hudString = hudString.Insert(index, "\n");
-                gap = 16;
+                gap = 15;
                 index = hudString.TakeWhile(c => (gap -= (c == '\n' ? 1 : 0)) > 0).Count();
                 hudString = hudString.Insert(index + 1, "\n");
-                gap = 20;
+                gap = 22;
                 index = hudString.TakeWhile(c => (gap -= (c == '\n' ? 1 : 0)) > 0).Count();
                 hudString = hudString.Insert(index + 1, "\n");
             } else if (counter == 2) {
